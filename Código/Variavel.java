bae8149a53ca222784102a467855a39627e01a1b
JacielleMarques <jacymarques71@gmail.com>
2022-03-08 16:21:29 -0300

    codigo implementado na aula do dia 03/03

---
+++ Código/Variavel.java
@@ -0,0 +1,6 @@
+public class Variavel {
+    public static void main(String[] args) {
+        System.out.println("Não se vai a geladera ta vazia ta faltando ate mistura, não se vai ");
+    }
+    
+}
